78d36e8f57f5977a4319f53ccfad95ee2b6d0311
unknown <muriraubach@gmail.com>
2021-12-06 18:36:27 -0300

    feat: delete a to do

--- src/index.js
+++ src/index.js
@@ -93,7 +93,18 @@ app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
 });
 
 app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+
+  const todo = user.todos.find((t) => t.id === id);
+
+  if (!todo) {
+    return response.status(404).json({ error: "To do not found!" });
+  }
+
+  user.todos.splice(todo, 1);
+
+  return response.status(204).json(todo);
 });
 
 module.exports = app;
